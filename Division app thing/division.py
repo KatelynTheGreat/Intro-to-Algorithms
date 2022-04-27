f7b8eb14c35840ad34bad7eaac049bbd7cb590a7
baggedmilk999 <89410031+baggedmilk999@users.noreply.github.com>
2022-04-27 12:32:47 -0400

    Add files via upload

---
+++ Division app thing/division.py
@@ -0,0 +1,5 @@
+numb1 = int(input("Enter a number\n"))
+numb2 = int(input("What do you want to divide it by?\n"))
+div = numb1/numb2
+yse = numb1%numb2
+print(f"The answer is {int(div)}, Remainder {yse}")
